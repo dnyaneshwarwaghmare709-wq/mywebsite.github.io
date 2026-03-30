defddcc2317189c5eb951f628fa7ec8247c82d61
dnyaneshwarwaghmare709-wq <dnyaneshwarwaghmare709@gmail.com>
2026-03-30 17:33:47 +0530

    Add CV download function and contact form handler

---
+++ script.js
@@ -0,0 +1,8 @@
+function downloadCV() {
+  alert("Add your CV file in the project folder and link it here!");
+}
+
+document.getElementById("contactForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Message sent! (Demo only)");
+});
